55934d04c5128965a9e168e069a438842b2196ae
wepasch <105050955+wepasch@users.noreply.github.com>
2025-02-01 19:48:30 +0100

    Update catppuccin.lua

--- nvim/lua/user/plugins/catppuccin.lua
+++ nvim/lua/user/plugins/catppuccin.lua
@@ -1,7 +1,8 @@
 return {
   "catppuccin/nvim",
+  as = "catppuccin",
   config = function()
     vim.cmd("colorscheme catppuccin-mocha")
-    vim.cmd("highlight Cursor guifg=#282828 guibg=#990000")
+    vim.cmd("highlight Cursor guifg=#282828 guibg=#aa0000")
   end
 }
