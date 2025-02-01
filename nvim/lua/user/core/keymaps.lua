c3b780ff6d85afa8cb56f6f26888066b690033b1
wepasch <105050955+wepasch@users.noreply.github.com>
2025-02-01 19:20:20 +0100

    Update keymaps.lua

--- nvim/lua/user/core/keymaps.lua
+++ nvim/lua/user/core/keymaps.lua
@@ -4,4 +4,4 @@ local keymap = vim.keymap
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 keymap.set("n", "<leader>nh", "nohl<CR>", { desc = "Clear seach highlights" }) --not working 
 keymap.set("n", "<leader>+", "<C-x>", { desc = "inc by 1" })
-keymap.set("n", "<leader>+", "<C-a>", { desc = "dec by 1" })
+keymap.set("n", "<leader>-", "<C-a>", { desc = "dec by 1" })
